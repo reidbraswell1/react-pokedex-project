51b5d9f23301372130fab4654d98bdfe332e280b
reid <reidbras@gmail.com>
2022-09-28 21:21:28 -0500

    Removing heading and moving it to unordered list component

--- src/components/unorderedList.jsx
+++ src/components/unorderedList.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 
 const UnorderedList = (props) => {
 
-    return(<ul className="list-group">
-            {props.pokemons.map((value, index, array) => {
-                return (<li className="list-group-item" key={value.id} id={value.id}>
-                    {`${value.id}.`} {value.name}, {value.type}, '{value.weaknesses.toString()}'
-                    <img className="border border-rounded" style={{marginLeft:".5rem", padding:"2px"}} width="35rem" src={value.img}></img>
-                    </li>)
-                })
-            }
-        </ul>);
+    return(<div>
+                <h4 className="text-center color-white">Pokemon Number Name Type Weaknesses</h4>
+                <ul className="list-group">
+                    {props.pokemons.map((value, index, array) => {
+                        return (<li className="list-group-item" key={value.id} id={value.id}>
+                                {`${value.id}.`} {value.name}, {value.type}, '{value.weaknesses.toString()}'
+                                <img className="border border-rounded" style={{marginLeft:".5rem", padding:"2px"}} width="35rem" src={value.img}></img>
+                                </li>)
+                            })
+                    }
+                </ul>
+            </div>);
 }
 export default UnorderedList;
