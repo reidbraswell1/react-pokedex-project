05c908172fd4a441942cbd5fa9b3ef722df7bdcf
reid <reidbras@gmail.com>
2022-10-01 10:53:22 -0500

    Adding Details view

---
+++ src/views/Details.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+const DetailsView = (props) => {
+    return(<div className="container-fluid">
+                <div className="row">
+                    <div className="col-4 my-center">
+                        <h1 className="color-white">Pokemon Details</h1>
+                    </div>
+                </div>
+            </div>)
+}
+export { DetailsView };
