b129b7edc5d0b2043a8e140ca4959d500725eed3
reid <reidbras@gmail.com>
2022-10-01 10:52:46 -0500

    Removing code that checked for undifined and instead populated state variables with initial values.

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 
 const Table = (props) => {
+
+    console.log(`---Begin Function Table()---`);
+    console.log(`Props=`,props);
+    console.log(`---End Function Table()---`);
+
     return(<table className="table background-color-white">
         <caption style={{captionSide:"top"}}>Table of Pokemons</caption>
         <thead>
@@ -14,25 +19,11 @@ const Table = (props) => {
         </thead>
         <tbody>
         {props.pokemons.map((value, index, array) => {
-                let weaknesses;
-                if(typeof value.weaknesses === "undefined") {
-                    weaknesses="";
-                }
-                else
-                    weaknesses=value.weaknesses.toString();
-                
-                let types;
-                if(typeof value.type === "undefined") {
-                    types="";
-                }
-                else
-                    types=value.type.toString();
-                
                 return (<tr id={value.id}>
-                            <td scope="row">{value.num}</td>
+                            <td>{value.num}</td>
                             <td>{value.name}</td>
-                            <td>{types}</td>
-                            <td>{weaknesses}</td>
+                            <td>{value.type.toString()}</td>
+                            <td>{value.weaknesses.toString()}</td>
                             <td>
                                 <Link className="" to={`/Details/${value.id}`}>
                                 <img className="border rounded imgLink" style={{marginLeft:".5rem", padding:"2px"}} src={value.img} width="40rem"></img>
@@ -43,6 +34,6 @@ const Table = (props) => {
                 })
             }
         </tbody>
-    </table>)
+    </table>);
 }
 export default Table;
