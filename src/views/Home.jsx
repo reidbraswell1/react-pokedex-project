ff6c1934da842bcda0cfd5ef4214870a2c2b141e
reid <reidbras@gmail.com>
2022-10-01 10:57:12 -0500

    Updating Home page

---
+++ src/views/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { processSubmission } from "../utils/pokemonUtils";
+
+const HomePage = (props) => {
+
+    console.log(`---Begin Function HomePage()---`);
+    console.log(`Props`,props);
+    console.log(`---End Function HomePage()---`);
+
+    return(<div className="container-fluid">
+                <div className="row">
+                    <div className="col-4 my-center border rounded pb-3 pt-3">
+                        <form className="" onSubmit={processSubmission}>
+                            <div className="form-group">
+                                <label className="color-white" htmlFor="pokemonName">Pokemon Name</label>
+                                <select className="form-select" id="pokemon-name" name="pokemonName" aria-label="Pokemon Name" multiple="true">
+                                    <option id="pokemon-name-all" value="All" selected="true">All</option>
+                                    { props.pokemonNames.map((value, index, array) => {
+                                        return(<option id={`pokemon-name-${index}`} value={value.id}>{value.name}</option>)
+                                    })}
+                                </select>
+                                <p>Ctrl Selects Multiple Values</p>
+                            </div>
+                            <div className="form-group">
+                                <label className="color-white" for="pokemonType">Pokemon Type</label>
+                                <select className="form-select" id="pokemon-type" name="pokemonType" aria-label="Pokemon Type" multiple="true">
+                                    <option id="pokemon-type-all" value="All" selected="true">All</option>
+                                    { props.pokemonTypes.map((value, index, array) => {
+                                        return(<option id={`pokemon-type-${index}`} value={value}>{value}</option>)
+                                    })}
+                                </select>
+                                <p>Ctrl Selects Multiple Values</p>
+                            </div>
+                            <div className="form-group">
+                                <label className="color-white" for="pokemonWeakness">Pokemon Weakness</label>
+                                <select className="form-select" id="pokemon-weakness" name="pokemonWeakness" aria-label="Pokemon Weakness" multiple="true">
+                                    <option id="pokemon-weakness-all" value="All" selected="true">All</option>
+                                    { props.pokemonWeaknesses.map((value, index, array) => {
+                                        return(<option id={`pokemon-weakness-${index}`} value={value}>{value}</option>)
+                                    })}
+                                </select>
+                                <p>Ctrl Selects Multiple Values</p>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>)
+}
+export { HomePage };
