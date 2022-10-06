a258812fc36b0edb1ba1075a5597cc9dcb33b4d4
Reid Braswell <reidbras@gmail.com>
2022-10-06 17:52:49 -0500

    Updating table keys and adding sort logic

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Table = (props) => {
@@ -6,17 +6,43 @@ const Table = (props) => {
     console.log(`---Begin Function Table()---`);
     console.log(`Props=`, props);
 
+    const [pokemon, setPokemon] = useState(props.pokemons);
+
     useEffect(() => {
 
-    })
+    },[])
 
     const processSort = (event) => {
         console.log(`Begin Function processSort()---`);
-        console.log(`Event.id=`,event.target.id);
+        console.log(`Event.target.id =`,event.target.id);
         switch(event.target.id) {
             case "numeric-sort-down":
+                let nbrDown = props.pokemons.slice(0);
+                nbrDown.sort((a, b) => {
+                    return b.id < a.id;
+                });
+                setPokemon(nbrDown);
                 break;
             case "numeric-sort-up":
+                let nbrUp = props.pokemons.slice(0);
+                nbrUp.sort((a, b) => {
+                    return b.num > a.num;
+                });
+                setPokemon(nbrUp);
+                break;
+            case "alpha-sort-down":
+                let alphaDown = props.pokemons.slice(0);
+                alphaDown.sort((a, b) => {
+                    return b.name < a.name;
+                });
+                setPokemon(alphaDown);
+                break;
+            case "alpha-sort-up":
+                let alphaUp = props.pokemons.slice(0);
+                alphaUp.sort((a, b) => {
+                    return b.name > a.name;
+                });
+                setPokemon(alphaUp);
                 break;
             default:
                 break;
@@ -40,7 +66,7 @@ const Table = (props) => {
                 <td>
                     <div className="w-12">
                         <button value="numericSortDown" onClick={processSort}>
-                            <i id="numeric-sort-down" class="bi bi-sort-numeric-down"></i>
+                            <i id="numeric-sort-down" class="bi bi-sort-numeric-down" ></i>
                         </button>
                         <button value="numericSortUp" onClick={processSort}>
                             <i id="numeric-sort-up" class="bi bi-sort-numeric-up-alt"></i>
@@ -48,11 +74,11 @@ const Table = (props) => {
                     </div>
                 </td>
                 <td>
-                    <button>
-                        <i className="bi bi-sort-alpha-down"></i>
+                    <button onClick={processSort}>
+                        <i id="alpha-sort-down" className="bi bi-sort-alpha-down"></i>
                     </button>
-                    <button>
-                        <i className="bi bi-sort-alpha-up-alt"></i>
+                    <button onClick={processSort}>
+                        <i id="alpha-sort-up" className="bi bi-sort-alpha-up-alt"></i>
                     </button>
                 </td>
                 <td></td>
@@ -61,13 +87,13 @@ const Table = (props) => {
             </tr>
         </thead>
         <tbody>
-            {props.pokemons.map((value, index, array) => {
-                return (<tr key={`results-details-${index}`} id={value.id}>
-                    <td key={index}>{value.num}</td>
-                    <td key={index}>{value.name}</td>
-                    <td key={index}>{value.type.toString()}</td>
-                    <td key={index}>{value.weaknesses.toString()}</td>
-                    <td key={index}>
+            {pokemon.map((value, index, array) => {
+                return (<tr key={`results-details-tr-${index}`} id={`results-details-tr-${index}`}>
+                    <td key={`results-details-td-1`}>{value.num}</td>
+                    <td key={`results-details-td-2`}>{value.name}</td>
+                    <td key={`results-details-td-3`}>{value.type.toString()}</td>
+                    <td key={`results-details-td-4`}>{value.weaknesses.toString()}</td>
+                    <td key={`results-details-td-5`}>
                         <Link className="" to={`/Details/${value.id}`} state={{ "pokemon": value, "pokemonImages": props.pokemonImages }}>
                             <img className="border rounded imgLink mx-auto d-block" style={{ marginLeft: ".5rem", padding: "2px" }} src={value.img} alt="Pokemon" width="40rem" loading="lazy"></img>
                         </Link>
