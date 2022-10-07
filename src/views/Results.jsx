e2d47e37b9908943cb3676c2667e4763a44d0a13
reid <reidbras@gmail.com>
2022-10-07 12:30:40 -0500

    Adding display of seacrh criteria

--- src/views/Results.jsx
+++ src/views/Results.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import  Table  from "../components/table.jsx";
+import Table from "../components/table.jsx";
 import Footer from "../components/footer.jsx";
 import { useEffect } from "react";
 
@@ -10,46 +10,53 @@ const Results = (props) => {
 
     const location = useLocation();
     const navigate = useNavigate();
-    console.log(`Location.state=`,location)
+    console.log(`Location.state=`, location)
 
     useEffect(() => {
         console.log(`---Begin Function useEffect()---`);
-        if(location.state === null) {
+        if (location.state === null) {
             navigate("/");
         }
         console.log(`---End Function useEffect()---`)
-    },[]);
-    
+    }, []);
+
     console.log(`---End Function Results()---`);
 
-    return(
-        <div className="container">
+    return (
+        <div className="container-fluid">
             <div className="row">
-                <div className="col-4 my-center">
+                <div className="col-4 mx-auto">
                     <h1 className="color-white text-center">Pokemon List</h1>
                 </div>
             </div>
             <div className="row">
-                <div className="col-5 my-center">
-                    { location.state != null && 
+                <div className="col-7 mx-auto">
+                    {location.state != null &&
                         <Table pokemons={location.state.pokemons} pokemonImages={location.state.pokemonImages}></Table>
                     }
-                    { location.state != null &&
-                        <div>
-                            <p className="error"><span className="color-red">{location.state.errorText}</span></p>
-                            <p className="error"><span className="color-red">{location.state.errorNames}</span></p>
-                            <p className="error"><span className="color-red">{location.state.errorTypes}</span></p>
-                            <p className="error"><span className="color-red">{location.state.errorWeaknesses}</span></p>
+                    <div className="row">
+                        <div className="col-7 mx-auto">
+                            {location.state != null && location.state.errorText.length > 0 &&
+                                <div>
+                                    <p className="error"><span className="color-red">{location.state.errorText}</span></p>
+                                    <p className="error"><span className="color-red">{location.state.filterNames}</span></p>
+                                    <p className="error"><span className="color-red">{location.state.filterTypes}</span></p>
+                                    <p className="error"><span className="color-red">{location.state.filterWeaknesses}</span></p>
+                                </div>
+                            }
+                            {location.state != null && location.state.filterText.length > 0 &&
+                                <div>
+                                    <p className="error"><span className="color-white">{location.state.filterText}</span></p>
+                                    <p className="error"><span className="color-white">{location.state.filterNames}</span></p>
+                                    <p className="error"><span className="color-white">{location.state.filterTypes}</span></p>
+                                    <p className="error"><span className="color-white">{location.state.filterWeaknesses}</span></p>
+                                </div>
+                            }
                         </div>
-                    }
-                </div>
-            </div>
-        
-            <div className="row">
-                <div className="col-5 my-center">
+                    </div>
                     <Footer></Footer>
                 </div>
-            </div>      
+            </div>
         </div>);
 }
-export {Results};
+export { Results };
