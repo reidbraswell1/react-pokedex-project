d7078a43ab0328791a992a8754c03e31836ab01d
reid <reidbras@gmail.com>
2022-10-11 12:56:05 -0500

    Removing results prop from results page. Results will be computed from parameters and passing the pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,8 @@ const App = (props) => {
           <Route exact path="/" element={<PokedexPage pokemonList={pokedexList}></PokedexPage>} end></Route>
         */}
           <Route exact path="/" element={<HomePage pokemonNames={names} pokemonTypes={types} pokemonWeaknesses={weaknesses} pokemonImages={images} pokemonList={pokedexList}></HomePage>} end></Route>
-          <Route path="/results/:ids/:types/:weaknesses" element={<Results pokedexList={pokedexList} props={props.results}></Results>}></Route>
+          {/*<Route path="/results/:ids/:types/:weaknesses" element={<Results pokedexList={pokedexList} props={props.results}></Results>}></Route>*/}
+          <Route path="/results/:ids/:types/:weaknesses" element={<Results pokedexList={pokedexList}></Results>}></Route>
           <Route path="/Details/:id" element={<DetailsView name="Details_Page" pokedexList={pokedexList}></DetailsView>}></Route>
       </Routes>
     </BrowserRouter>
