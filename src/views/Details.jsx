11772e06ca706f632d80d41f95b89c37b34cc61f
reid <reidbras@gmail.com>
2022-10-13 13:50:46 -0500

    Refactoring to use parameters

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -2,6 +2,7 @@ import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
 import { React, useEffect, useState } from "react";
 import { Navigate, Redirect, useLocation, useParams, useNavigate } from "react-router-dom";
 import Footer from "../components/footer";
+import { getPokedexList } from "../utils/pokemonUtils";
 
 let mySeconds = 6;
 let timeout = null;
@@ -9,36 +10,105 @@ let timeout = null;
 const DetailsView = (props) => {
 
     console.log(`---Begin Function DetailsView()---`);
-    console.log(`Props.pokedexList =`,props.pokedexList);
 
     const params = useParams();
 
-    console.log(`Params.id =`,params.id)
     const location = useLocation();
     const navigate = useNavigate();
 
-    const [ seconds, setSeconds ] = useState(6);
-    console.log(props.pokedexList.pokemon[params.id]);
-    
+    console.log(`Props =`, props);
+    console.log(`Params =`, params);
+    console.log(`Location =`, location);
 
-    let pokemon = props.pokedexList.pokemon[params.id];
-    let pokemons = props.pokedexList.pokemon;
-    let pokemonImages = null;
-    
-    if (location.state != null) {
-        if ('pokemon' in location.state) {
-    //        pokemon = location.state.pokemon;
-        }
-        if ('pokemonImages' in location.state) {
-            pokemonImages = location.state.pokemonImages;
-        }
+    const [pokemon, setPokemon] = useState([]);
+    const [pokemons, setPokemons] = useState([]);
+
+    const [seconds, setSeconds] = useState(6);
+
+    /*
+    let pokemons = [];
+    if('pokemon' in props.pokedexList) {
+        pokemons = props.pokedexList.pokemon;
+    }
+    */
+    if ('pokemon' in props.pokedexList) {
+        //setPokemons(props.pokedexList.pokemon);
     }
 
+    /*
+    let pokemon=[];
+    if(location.state !== null) {
+        if('pokemon' in location.state) {
+            pokemon = location.state.pokemon;
+        }
+    }
+    else {
+        getPokedexList()
+        .then((results) => {
+            if ('data' in results) {
+                if (results.data === null) {
+                    console.log(`Results.data is null =`, results.data)
+                }
+                else {
+                    console.log(`Results.data not null =`, results.data);
+                    if ('pokemon' in results.data) {
+                        console.log(`Results.data.pokemon =`, results.data.pokemon);
+                        console.log("Params.id=",params.id);
+                        pokemons = (results.data.pokemon);
+                        pokemons.forEach((obj, idx, array) => {
+                            if(obj.id.toString() === params.id.toString()) {
+                                pokemon.push(obj);
+                            }
+                        })
+                        console.log("Pokemon=",pokemon);
+                    }
+                    else {
+                        console.log("Pokemon not in results");
+                    }
+                }
+            }
+            else {
+                console.log("Data not in results");
+            }
+        }) 
+
+    }
+    */
+
     console.log(`Pokemon`, pokemon);
-    console.log(`PokemonImages`, pokemonImages);
 
     useEffect(() => {
         console.log(`---Begin Function useEffect()---`);
+        getPokedexList()
+            .then((results) => {
+                if ('data' in results) {
+                    if (results.data === null) {
+                        console.log(`Results.data is null =`, results.data)
+                    }
+                    else {
+                        console.log(`Results.data not null =`, results.data);
+                        if ('pokemon' in results.data) {
+                            console.log(`Results.data.pokemon =`, results.data.pokemon);
+                            console.log("Params.id=", params.id);
+                            const reslt = (results.data.pokemon);
+                            reslt.forEach((obj, idx, array) => {
+                                if (obj.id.toString() === params.id.toString()) {
+                                    //pokemon.push(obj);
+                                    setPokemon(obj);
+                                }
+                            })
+                            setPokemons(reslt);
+                            console.log("Pokemon=", pokemon);
+                        }
+                        else {
+                            console.log("Pokemon not in results");
+                        }
+                    }
+                }
+                else {
+                    console.log("Data not in results");
+                }
+            })
 
         /*
         if (location.state === null) {
@@ -59,100 +129,109 @@ const DetailsView = (props) => {
 
     mySeconds = seconds;
     const startInterval = () => {
-        
-          timeout = setInterval(() => {
+
+        timeout = setInterval(() => {
             setSeconds((seconds) => seconds - 1);
             //mySeconds = mySeconds - 1
             console.log("seconds: ", seconds); // seconds always return 6 but mySeconds the updated value
-            console.log("myseconds: ",mySeconds);
-            if(mySeconds < 1) {
+            console.log("myseconds: ", mySeconds);
+            if (mySeconds < 1) {
                 clearInterval(timeout);
                 navigate("/");
             }
-          },1000);
+        }, 1000);
     }
-  
+
 
     return (
         <div className="container-fluid">
-                <div>
-                    <div className="row">
-                        <div className="col-4 mx-auto">
-                            <h1 className="color-white text-center">{pokemon.name} Details</h1>
-                        </div>
+            <div>
+                <div className="row">
+                    <div className="col-4 mx-auto">
+                        <h1 className="color-white text-center">{pokemon.name}Details</h1>
                     </div>
-                    <div className="row">
-                        <div className="col-sm">
-                            <div class="row">
-                                <div class="col-7 mx-auto">
-                                    <img className="mx-auto d-block" src={pokemon.img} alt="Pokemon"></img>
-                                    <table className="table table-sm table-striped table-bordered background-color-white caption-top">
-                                        <caption className="color-white text-center">Property Value</caption>
-                                        <thead>
-                                            <tr key="property-value-table">
-                                                <th scope="col">Property</th>
-                                                <th scope="col">Value</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <div class="row">
+                            <div class="col-7 mx-auto">
+                                <img className="mx-auto d-block" src={pokemon.img} alt="Pokemon"></img>
+                                <table className="table table-sm table-striped table-bordered background-color-white caption-top">
+                                    <caption className="color-white text-center">Property Value</caption>
+                                    <thead>
+                                        <tr key="property-value-table">
+                                            <th scope="col">Property</th>
+                                            <th scope="col">Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
                                         {Object.keys(pokemon).map((key, index, array) => {
-                                            console.log(`Key =`,key, `Value =`,pokemon[key])
-                                                return (<tr key={`tr-${index}`}>
-                                                    <td key={`td-${index}-1`} className="first-letter">{key}:</td>
-                                                    <td key={`td-${index}-2`}>{pokemon[key] === null ? "Null": typeof pokemon[key] === "object" ? JSON.stringify(pokemon[key]): pokemon[key].toString()}</td>
-                                                </tr>)
-                                            })}
-                                        </tbody>
-                                    </table>
-                                    <div className="row">
-                                        <div className="col-5 my-center">
-                                            {('next_evolution' in pokemon || 'prev_evolution' in pokemon) &&
-                                                <table className="table table-sm table-striped table-bordered background-color-white caption-top">
+                                            console.log(`Key =`, key, `Value =`, pokemon[key])
+                                            return (<tr key={`tr-${index}`}>
+                                                <td key={`td-${index}-1`} className="first-letter">{key}:</td>
+                                                <td key={`td-${index}-2`}>{pokemon[key] === null ? "Null" : typeof pokemon[key] === "object" ? JSON.stringify(pokemon[key]) : pokemon[key].toString()}</td>
+                                            </tr>)
+                                        })}
+                                    </tbody>
+                                </table>
+                                <div className="row">
+                                    <div className="col-5 my-center">
+                                        {('next_evolution' in pokemon || 'prev_evolution' in pokemon) &&
+                                            <table className="table table-sm table-striped table-bordered background-color-white caption-top">
+                                                {'next_evolution' in pokemon &&
+                                                    <caption className="color-white text-center">Next Evolution:</caption>
+                                                }
+                                                {'prev_evolution' in pokemon &&
+                                                    <caption className="color-white text-center">Prev Evolution:</caption>
+                                                }
+                                                <thead>
+                                                    <tr key="heading-next-evolution">
+                                                        <th scope="col">Num</th>
+                                                        <th scope="col">Name</th>
+                                                        <th scope="col">Image</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className="">
                                                     {'next_evolution' in pokemon &&
-                                                        <caption className="color-white text-center">Next Evolution:</caption>
-                                                    }
+                                                        pokemon.next_evolution.map((obj1, index, array) => {
+                                                            let id = parseInt(obj1.num);
+                                                            console.log(`Id=`, id);
+                                                            return (<tr key={`heading-next-evolution-${index}`}>
+                                                                <td>{obj1.num}</td>
+                                                                <td>{obj1.name}</td>
+                                                                <td>{pokemons.map((obj2, idx, array) => {
+                                                                    if (obj2.id.toString() === obj1.id.toString()) { return <img className="mx-auto d-block" src={obj2.img} width="40rem" alt="Pokemon"></img> }
+                                                                })}
+                                                                </td>
+                                                            </tr>)
+                                                        })}
                                                     {'prev_evolution' in pokemon &&
-                                                        <caption className="color-white text-center">Prev Evolution:</caption>
-                                                    }
-                                                    <thead>
-                                                        <tr key="heading-next-evolution">
-                                                            <th scope="col">Num</th>
-                                                            <th scope="col">Name</th>
-                                                            <th scope="col">Image</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody className="">
-                                                        {'next_evolution' in pokemon &&
-                                                            pokemon.next_evolution.map((value, index, array) => {
-                                                                let id = parseInt(value.num);
-                                                                console.log(`Id=`, id);
-                                                                return (<tr key={`heading-next-evolution-${index}`}>
-                                                                    <td>{value.num}</td>
-                                                                    <td>{value.name}</td>
-                                                                    <td><img className="mx-auto d-block" src={pokemons[id].img} width="40rem" alt="Pokemon"></img></td>
-                                                                </tr>)
-                                                            })}
-                                                        {'prev_evolution' in pokemon &&
-                                                            pokemon.prev_evolution.map((value, index, array) => {
-                                                                let id = parseInt(value.num);
-                                                                console.log(`Id=`, id);
-                                                                return (<tr key={`heading-prev-evolution-${index}`}>
-                                                                    <td>{value.num}</td>
-                                                                    <td>{value.name}</td>
-                                                                    <td><img className="mx-auto d-block" src={pokemons[id].img} width="40rem" alt="Pokemon"></img></td>
-                                                                </tr>)
-                                                            })}
-                                                    </tbody>
-                                                </table>
-                                            }
-                                        </div>
+                                                        pokemon.prev_evolution.map((obj1, index, array) => {
+                                                            let id = parseInt(obj1.num);
+                                                            console.log(`Id=`, id);
+                                                            return (<tr key={`heading-prev-evolution-${index}`}>
+                                                                <td>{obj1.num}</td>
+                                                                <td>{obj1.name}</td>
+                                                                {pokemons.map((obj2, idx2, array) => {
+                                                                    console.log(`obj2.id=`, obj2.id)
+                                                                    console.log(`obj1 =`, obj1)
+                                                                    if (obj2.num.toString() === obj1.num.toString()) {
+                                                                        return <td><img className="mx-auto d-block" src={obj2.img} width="40rem" alt="Pokemon"></img></td>
+                                                                    }
+                                                                })}
+                                                            </tr>)
+                                                        })}
+                                                </tbody>
+                                            </table>
+                                        }
                                     </div>
                                 </div>
                             </div>
-                            <Footer></Footer>
                         </div>
+                        <Footer></Footer>
                     </div>
                 </div>
+            </div>
             {/*location.state === null &&
                 <div>
                     <h1>location.state is null redirecting to Home page in 
